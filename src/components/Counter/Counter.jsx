6f80a75bc7fc8c524a72d49302cf8b316636cf6b
MunnaKhan99 <munnakhan01329@gmail.com>
2025-08-22 19:52:24 +0600

    add localstorage to persistant data

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6";
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(()=>{
+        const saved = localStorage.getItem("counterValue");
+        return saved !== null ?Number(saved):0;
+    });
     const [step,setStep] = useState(1);
 
+    // after loading component the value will load form localstorage
+    // useEffect(()=>{
+    //     const savedCount = localStorage.getItem("counterValue");
+    //     if(savedCount !== null){
+    //         setCount(Number(savedCount));
+    //     }
+    // },[]);
+    useEffect(()=>{
+        localStorage.setItem("counterValue",count);
+    },[count]);
 
     const handleIncreament = () => {
         if(count + step <=100){
@@ -26,7 +39,8 @@ const Counter = () => {
         <div className=' flex justify-center items-center h-screen bg-gradient-to-r from-indigo-500 via-purple-500'>
             <div className='bg-white shadow-2xl p-10 w-96 text-center'>
                 <h1 className='text-3xl font-bold text-gray-800 m-6'>Counter App</h1>
-                <div className={` text-6xl font-extrabold mb-6 transition-all duration-300  ${count > 0 ? 'text-green-600' : count < 0 ? 'text-red-600' : 'text-gray-700'}`}>
+
+                <div className={` text-6xl font-extrabold mb-6 transition-all duration-300  ${count > 50 ? 'text-green-600' : count < 50 ? 'text-red-600' : 'text-gray-700'}`}>
                     {count}
 
                 </div>
@@ -34,7 +48,10 @@ const Counter = () => {
                 <input 
                 type="number"
                 value={step}
-                onChange={(e) => setStep(Number(e.target.value))}  
+                onChange={(e) =>{
+                    const value = e.target.value;
+                    setStep(value === "" ? 1: Number(value));
+                }}  
                 className='border p-2 mb-4 text-center'
                 placeholder="5"/>
 
@@ -49,7 +66,7 @@ const Counter = () => {
 
                     {/* Decrement button */}
                     <button onClick={handleIncreament}
-                    disabled={count - step > 100}
+                    disabled={count + step > 100}
                     className={`flex-1 flex items-center justify-center bg-green-500 hover:bg-green-600
                      text-white py-3 rounded-xl shadow-lg transition-all duration-300 ${count - step > 100 ? 'bg-gray-400 cursor-not-allowed':'bg-green-500 hover:bg-green-600'}`}><FaPlus /></button>
                 </div>
